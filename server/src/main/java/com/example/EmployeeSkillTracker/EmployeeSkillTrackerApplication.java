b563e9817a7e36717a5300c796c3572fc764c374
Obinna Mokwe <obinnamokwe@Obinnas-MacBook-Air.local>
2021-10-09 19:11:24 -0500

    changes saved successfully

---
+++ server/src/main/java/com/example/EmployeeSkillTracker/EmployeeSkillTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeSkillTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSkillTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSkillTrackerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/EmployeeSkillTracker/EmployeeSkillTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeSkillTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSkillTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
